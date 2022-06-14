56297731d770b2ca670f4399d72d022ffe9abe7e
enchance <enchance@gmail.com>
2022-06-14 16:19:37 +0800

    simplify GATEKEEPER by replacing it with OWNER instead

--- contracts/lib/Gatekeeper.sol
+++ contracts/lib/Gatekeeper.sol
@@ -5,34 +5,31 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 
 contract Gatekeeper is AccessControl {
-    bytes32 public GATEKEEPER = keccak256("GATEKEEPER");
+    bytes32 public OWNER = keccak256("OWNER");
 
     mapping(string => bytes32) public gkroles;
 
-    constructor(address owner, address[] memory admins, address[] memory staffs) {
+    constructor(address[] memory admins) {
         gkroles['ARENA_OWNER'] = keccak256("ARENA_OWNER");
         gkroles['ARENA_ADMIN'] = keccak256("ARENA_ADMIN");
         gkroles['ARENA_STAFF'] = keccak256("ARENA_STAFF");
         gkroles['ARENA_CONTRACT'] = keccak256("ARENA_CONTRACT");
 
-        _grantRole(GATEKEEPER, msg.sender);
-        _grantRole(gkroles['ARENA_OWNER'], owner);
-        _grantRole(gkroles['ARENA_ADMIN'], owner);
-        _grantRole(gkroles['ARENA_STAFF'], owner);
-        _grantRole(gkroles['ARENA_CONTRACT'], owner);
+        _grantRole(OWNER, msg.sender);
+        _grantRole(gkroles['ARENA_OWNER'], msg.sender);
+        _grantRole(gkroles['ARENA_ADMIN'], msg.sender);
+        _grantRole(gkroles['ARENA_STAFF'], msg.sender);
+        _grantRole(gkroles['ARENA_CONTRACT'], msg.sender);
 
         // Role admins
-        _setRoleAdmin(GATEKEEPER, GATEKEEPER);
-        _setRoleAdmin(gkroles['ARENA_OWNER'], GATEKEEPER);
+        _setRoleAdmin(OWNER, OWNER);
+        _setRoleAdmin(gkroles['ARENA_OWNER'], OWNER);
         _setRoleAdmin(gkroles['ARENA_ADMIN'], gkroles['ARENA_OWNER']);
         _setRoleAdmin(gkroles['ARENA_STAFF'], gkroles['ARENA_ADMIN']);
         _setRoleAdmin(gkroles['ARENA_CONTRACT'], gkroles['ARENA_OWNER']);
 
         for (uint i; i < admins.length; i++) {
             _grantRole(gkroles['ARENA_ADMIN'], admins[i]);
-        }
-        for (uint i; i < staffs.length; i++) {
-            _grantRole(gkroles['ARENA_STAFF'], staffs[i]);
         }
     }
 
